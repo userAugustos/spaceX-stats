cad1733ccc2bcebb5177de3aa3511b04dc695743
Felipe Augustos <felipe_augustos84@outlook.com.br>
2023-07-23 17:03:00 -0300

    feat: setting up for needed endpoints and tests

--- api/src/infra/http/express.ts
+++ api/src/infra/http/express.ts
@@ -1,18 +1,27 @@
 import express from 'express'
-import {index, listAll, listStats} from '../controller/LaunchesController'
-import {connect} from "../database/connect";
+import { config } from "dotenv";
+import {index, listAll, listStats, refreshData} from '@controller/LaunchesController'
+import {connect} from "@database/connect";
+import {hydrate} from '@cron/cron'
+
+config()
+
 
 const app = express()
+
 app.use(express.json())
 
 app.get('/', index)
 app.get('/launches', listAll)
 app.get('/launches/stats', listStats);
+app.get('/data', refreshData);
 
 (async () => {
   try{
-      await connect()
-      app.listen(3000, () => console.log("Server started on port 3000"));
+    await connect();
+    hydrate;
+
+    app.listen(3030, () => console.log("Server started on port 3030"));
   }catch (err: any) {
     console.error(err);
     process.exit(1);
